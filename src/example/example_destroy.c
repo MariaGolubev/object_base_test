68ac29f192f5068d7530000b9de6003c0cc3d994
Maria <mgolubev@student.codam.nl>
2025-01-18 22:57:45 +0100

    Clean name after destroy

--- src/example/example_destroy.c
+++ src/example/example_destroy.c
@@ -6,7 +6,7 @@
 /*   By: maria <maria@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/18 20:43:36 by maria         #+#    #+#                 */
-/*   Updated: 2025/01/18 21:51:34 by maria         ########   odam.nl         */
+/*   Updated: 2025/01/18 22:57:15 by maria         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@
 void	example_destroy(t_example *this)
 {
 	free(this->name);
+	this->name = NULL;
 }
